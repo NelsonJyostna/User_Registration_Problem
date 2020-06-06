544d80b3e778bd4d015294353843d20cbf22940e
nelson26-shruti-mom <nelsonwasekar555@gmail.com>
2020-06-06 21:12:37 +0530

    Added : Email

--- userreg.sh
+++ userreg.sh
@@ -34,7 +34,7 @@ read -p "Enter your email : " email
 
 emailpat="^[a-zA-Z0-9]+([._+-][0-9a-zA-Z]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{2,3}){1,2}$"
 
-if [[ $email = $emailpat ]]
+if [[ $email =~ $emailpat ]]
 then
         echo "Valid"
 else
