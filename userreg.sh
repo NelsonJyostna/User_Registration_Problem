953410701d7ee6aaa298b491a55c0fcc7934fd64
nelson26-shruti-mom <nelsonwasekar555@gmail.com>
2020-06-06 20:25:19 +0530

    Added : First Name Using If else

--- userreg.sh
+++ userreg.sh
@@ -1,1 +1,43 @@
-echo "Welcome"
+#!/bin/bash -x
+
+shopt -s extglob
+
+echo "Welcome to User Registration Problem"
+
+read -p "Enter your firstname : " firstname
+
+
+firstnamepat="^[A-Z]{1}[a-z]{2,}$"
+
+if [[ $firstname =~ $firstnamepat ]]
+then
+        echo "Valid"
+else
+        echo "Your name must start with one uppercase and minimum 3 characters"
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
