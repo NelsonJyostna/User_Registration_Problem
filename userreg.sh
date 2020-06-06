1ae8cbf30fb526bdee4107462e85fea32240ce31
nelson26-shruti-mom <nelsonwasekar555@gmail.com>
2020-06-06 20:37:01 +0530

    Added : Last Name using If Else

--- userreg.sh
+++ userreg.sh
@@ -6,16 +6,27 @@ echo "Welcome to User Registration Problem"
 
 read -p "Enter your firstname : " firstname
 
-
 firstnamepat="^[A-Z]{1}[a-z]{2,}$"
 
 if [[ $firstname =~ $firstnamepat ]]
 then
         echo "Valid"
 else
-        echo "Your name must start with one uppercase and minimum 3 characters"
+        echo "Your First name must start with one uppercase and minimum 3 characters"
 fi
 
+
+
+read -p "Enter your lastname : " lastname
+
+lastnamepat="^[A-Z]{1}[a-z]{2,}$"
+
+if [[ $lastname =~ $lastnamepat ]]
+then
+        echo "Valid"
+else
+        echo "Your Last name must start with one uppercase and minimum 3 characters"
+fi
 
 
 
